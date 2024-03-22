a2fe8feae52ae8ab202b77645510ec78da178cfb
Charlie Cárdenas <cacardenas7@hotmail.com>
2024-03-22 17:23:52 -0500

    Initialize project structure adhering to SOLID principles, adding core, shared, and products modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
+import { ProductItemComponent } from './products/components/product-item/product-item.component';
+import { ProductFormComponent } from './products/components/product-form/product-form.component';
+import { ProductEditComponent } from './products/components/product-edit/product-edit.component';
+import { ProductDeleteComponent } from './products/components/product-delete/product-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductFormComponent,
+    ProductEditComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
