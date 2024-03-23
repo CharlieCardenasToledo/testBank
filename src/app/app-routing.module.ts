7b64a428976ad7d3fed6c96b5996230b59464c67
Charlie Cárdenas <cacardenas7@hotmail.com>
2024-03-23 13:10:42 -0500

    feat: Set Up Routing for Product Management Views
    Establish a routing module in the application to handle navigation between the product list and product form views. The routes are defined to direct the root path to the ProductListComponent, which displays all products. Two child routes are created under 'producto' to handle product addition and editing, both routing to ProductFormComponent with the latter including a dynamic segment for the product ID.
    
    This routing configuration enables a clear and structured navigation experience within the app, allowing users to easily access different parts of the product management system.
    
    By incorporating these routes, we lay the groundwork for seamless transitions between listing products and managing individual product details, thereby enhancing the user interface and flow of the application.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './products/pages/product-list/product-list.component';
+import { ProductFormComponent } from './products/pages/product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  {
+    path: 'producto',
+    children: [
+      { path: 'agregar', component: ProductFormComponent },
+      { path: 'editar/:id', component: ProductFormComponent }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
