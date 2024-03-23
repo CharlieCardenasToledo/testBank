6995b2e06ca8f247c31a80e9497e88b26465c1af
Charlie Cárdenas <cacardenas7@hotmail.com>
2024-03-23 11:33:59 -0500

    feat: introduce FormValidationService with custom validators
    This commit introduces the FormValidationService, equipped with several custom validators to enhance form validation logic within Angular applications. The service provides the following validators:
    
    - dateRevisionAfterDateReleaseValidator: Ensures that a given review date is subsequent to the release date. This group-level validator is crucial for forms where the relationship between two dates needs to be validated, such as in product launch forms or project timelines.
    
    - alidateCurrentOrFutureDate: Validates that a specified date is not in the past, applicable to individual form controls. This validator is essential for ensuring that dates for events, deadlines, or any future-oriented actions are current or future dates.
    
    - dateNotPast: Another variant for validating that a specific date within a form group is not past the current date. This validator targets individual controls based on their name within a FormGroup, providing flexibility in validating dates across different forms.
    
    These validators aim to address common form validation scenarios, ensuring data integrity and user input validity. By implementing these validators, the application can enforce specific business rules directly within the form logic, improving usability and data quality.

---
+++ src/app/services/form-validation/form-validation.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormValidationService {
+  constructor() { }
+  /**
+   * Valida que la fecha de revisión sea posterior a la fecha de lanzamiento.
+   * Este validador se aplica a nivel de grupo de formulario, permitiendo acceder a múltiples controles del formulario.
+   * 
+   * @returns Una función validadora que verifica si la fecha de revisión es posterior a la de lanzamiento.
+   */
+  dateRevisionAfterDateReleaseValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const dateRelease = control.get('date_release')?.value;
+      const dateRevision = control.get('date_revision')?.value;
+      return dateRelease && dateRevision && dateRevision > dateRelease ? null : { 'revisionAfterRelease': true };
+    };
+  }
+  /**
+   * Valida que una fecha no sea anterior a la fecha actual.
+   * 
+   * @returns Una función validadora que verifica si una fecha dada es actual o futura.
+   */
+  validateCurrentOrFutureDate(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const today = new Date();
+      const dateValue = new Date(control.value);
+      return dateValue < today ? { 'dateNotFuture': true } : null;
+    };
+  }
+  /**
+   * Verifica que una fecha específica no sea del pasado.
+   * Este validador es específico para controles individuales dentro de un FormGroup, identificados por su nombre.
+   * 
+   * @param controlName El nombre del control a validar dentro del FormGroup.
+   * @returns Una función validadora que impone que la fecha no sea pasada.
+   */
+  dateNotPast(controlName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control instanceof FormGroup) {
+        const formControl = control.controls[controlName];
+        const currentDate = new Date().toISOString().split('T')[0];
+
+        if (formControl && formControl.value < currentDate) {
+          formControl.setErrors({ dateNotPast: true });
+        } else if (formControl && formControl.errors && !formControl.errors['required']) {
+          formControl.setErrors(null);
+        }
+      }
+      return null; 
+    };
+  }
+}
